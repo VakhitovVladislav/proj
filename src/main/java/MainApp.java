bca476ac12a4ef572e276aa712025ba74272727f
Vladislav Vakhitov <siemiens11@gmail.com>
2025-08-10 15:05:07 +0300

    изменена логи doSomething

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,13 @@
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(11111111111);
+        System.out.println(1001);
+        System.out.println(2222);
+        System.out.println(3333);
+    }
+    public static void dosomething() {
+        System.out.println(4444);
+        System.out.println(5555);
+        System.out.println(6666);
     }
 }
