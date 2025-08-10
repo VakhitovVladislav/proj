1939db048392b195a7a2eb08721da45bc530c793
Vladislav Vakhitov <siemiens11@gmail.com>
2025-08-10 15:09:37 +0300

    улучшили логику MainApp and doSomething

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,12 +2,12 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(1001);
-        System.out.println(2222);
-        System.out.println(3333);
+        System.out.println("--");
+
     }
-    public static void dosomething() {
-        System.out.println(4444);
-        System.out.println(5555);
-        System.out.println(6666);
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
