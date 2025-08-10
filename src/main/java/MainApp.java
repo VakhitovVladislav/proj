16062849861dadfdac8a5371606bf5bc2ed6c942
Vladislav Vakhitov <siemiens11@gmail.com>
2025-08-10 14:57:45 +0300

    fix git integration v2

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,6 @@
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(1111111111);
+        System.out.println(11111111111);
     }
 }
